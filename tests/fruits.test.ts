a48552170b26dba2a12bf4c723a16cf188b45ae6
gabriela <gabrielamontanhini@gmail.com>
2023-09-29 17:28:31 -0300

    Test: post e get fruits

---
+++ tests/fruits.test.ts
@@ -0,0 +1,35 @@
+import supertest from "supertest";
+
+import app from "server";
+import { FruitInput } from "services/fruits-service";
+
+const api = supertest(app)
+
+describe("/POST fruits", () => {
+    it("should return status 201 when creating a fruit", async () => {
+        const fruit: FruitInput = {
+            name: "Morgando",
+            price: 1
+          };
+      
+          const { status } = await api.post("/fruits").send(fruit);
+          expect(status).toBe(201);
+    })
+})
+
+describe("/GET fruits", () => {
+    it("should return all fruits", async () => {
+
+
+
+        const { status, body } = await api.get("/fruits");
+        expect(status).toBe(200);
+        expect(body).toEqual(expect.arrayContaining([
+            expect.objectContaining({
+              id: expect.any(Number),
+              name: expect.any(String),
+              price: expect.any(Number)
+            })
+          ]))
+    })
+})
